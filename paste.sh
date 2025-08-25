370a055334ea03034cb274ddb03a19804cd3900b
Matt Day <mattday@tenstorrent.com>
2025-08-25 15:20:01 -0500

    remove extra carriage return

--- paste.sh
+++ paste.sh
@@ -4,4 +4,4 @@
 # xclip -o defaults to the mouse selection
 # if there is none, then use the clipboard
 CLIP=$(xclip -o 2>/dev/null || xclip -o -selection clipboard 2>/dev/null)
-xdotool type --clearmodifiers --delay 0 --file <(echo "$CLIP")
+xdotool type --clearmodifiers --delay 0 --file <(echo -n "$CLIP")
